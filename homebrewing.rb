af17bb11775d984fb7f028521683c7103c838b54
Kabir <kabir@kabirkuriyan.com>
2018-06-01 14:17:09 -0400

    Adding formula

---
+++ homebrewing.rb
@@ -0,0 +1,15 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Homebrewing < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/thekabistro/homebrewing/archive/v1.0.1.tar.gz"
+  sha256 "91b21c56de86941486a86a57c5bbbaa701a50d4843696c817fa42f959d36514d"
+  # depends_on "cmake" => :build
+
+  def install
+     bin.install "myscript.sh"
+     bin.install "testfile.py"
+  end
+end
